e1fb8ea844db0922804443a5daf52f9b609b55bd
Lynn <lynn.janzen@posteo.de>
2020-09-27 16:20:40 +0200

    filter test

--- Bandpass_test.c
+++ Bandpass_test.c
@@ -12,59 +12,92 @@ typedef struct lowpass {
     float omega_c;
     float kappa;
     float delta;
-    float a_0;
-    float a_1; 
-    float a_2;
-    float b_1;
-    float b_2;
+    float a[3];
+    float b[2];
 } Lowpass;
-/*
-typedef struct highpass {
-    float f_c = 0.0;
-    float theta_c = M_PI*f_c/F_S;
-    float omega_c = M_PI*f_c;
-    float kappa = omega_c/tan(theta_c);
-    float delta = pow(kappa) + pow(omega_c) + 2*kappa*omega_c;
-    float a_0 = pow(kappa)/delta;
-    float a_1 = (-2*pow(kappa))/delta; 
-    float a_2 = pow(kappa)/delta;
-    float b_1 = (-2*pow(kappa)+2*pow(omega_c))/delta;
-    float b_2 = (-2*kappa*omega_c + pow(kappa) + pow(omega_c))/delta
-} Highpass;
-*/
 
-int main (){
-    
-    Lowpass instance_lowpass;
-     
-    printf("fc für lowpass eingeben: \n");
-    scanf("%f", &instance_lowpass.f_c);
-    instance_lowpass.theta_c = M_PI*instance_lowpass.f_c/F_S;
-    instance_lowpass.omega_c = M_PI*instance_lowpass.f_c;
-    instance_lowpass.kappa = instance_lowpass.omega_c/tan(instance_lowpass.theta_c);
-    instance_lowpass.delta = pow(instance_lowpass.kappa,2) + pow(instance_lowpass.omega_c,2) + 2*instance_lowpass.kappa*instance_lowpass.omega_c;
-    instance_lowpass.a_0 = pow(instance_lowpass.omega_c,2)/instance_lowpass.delta;
-    instance_lowpass.a_1 = 2*pow(instance_lowpass.omega_c,2)/instance_lowpass.delta; 
-    instance_lowpass.a_2 = pow(instance_lowpass.omega_c,2)/instance_lowpass.delta;
-    instance_lowpass.b_1 = (-2*pow(instance_lowpass.kappa,2)+2*pow(instance_lowpass.omega_c,2))/instance_lowpass.delta;
-    instance_lowpass.b_2 = (-2*instance_lowpass.kappa*instance_lowpass.omega_c + pow(instance_lowpass.kappa,2) + pow(instance_lowpass.omega_c,2))/instance_lowpass.delta;
-    printf("%f\n", instance_lowpass.a_0);
-    printf("%f\n", instance_lowpass.a_1);
-    printf("%f\n", instance_lowpass.a_2);
-    printf("%f\n", instance_lowpass.b_1);
-    printf("%f\n", instance_lowpass.b_2);
-    
+typedef struct highpass {
+    float f_c; 
+    float theta_c;
+    float omega_c; 
+    float kappa;
+    float delta; 
+    float a[3];
+    float b[2];
+} Highpass;
+
+
+void calc_lowpass (Lowpass* lp){
+
+    lp->theta_c = M_PI*lp->f_c/F_S;
+    lp->omega_c = M_PI*lp->f_c;
+    lp->kappa = lp->omega_c/tan(lp->theta_c);
+    lp->delta = pow(lp->kappa,2) + pow(lp->omega_c,2) + 2*lp->kappa*lp->omega_c;
+    lp->a[0] = pow(lp->omega_c,2)/lp->delta;
+    lp->a[1] = 2*pow(lp->omega_c,2)/lp->delta; 
+    lp->a[2] = pow(lp->omega_c,2)/lp->delta;
+    lp->b[0] = (-2*pow(lp->kappa,2)+2*pow(lp->omega_c,2))/lp->delta;
+    lp->b[1] = (-2*lp->kappa*lp->omega_c + pow(lp->kappa,2) + pow(lp->omega_c,2))/lp->delta;
 
 }
 
+void calc_highpass (Highpass* hp){
 
-// float f_c = 0.0;
-    // float theta_c = M_PI*f_c/F_S;
-    // float omega_c = M_PI*f_c;
-    // float kappa = omega_c/tan(theta_c);
-    // float delta = pow(kappa) + pow(omega_c) + 2*kappa*omega_c;
-    // float a_0 = pow(omega_c)/delta;
-    // float a_1 = 2*pow(omega_c)/delta; 
-    // float a_2 = pow(omega_c)/delta;
-    // float b_1 = (-2*pow(kappa)+2*pow(omega_c))/delta;
-    // float b_2 = (-2*kappa*omega_c + pow(kappa) + pow(omega_c))/delta
+    hp->theta_c = M_PI * hp->f_c / F_S;
+    hp->omega_c = M_PI * hp->f_c;
+    hp->kappa = hp->omega_c/tan(hp->theta_c);
+    hp->delta = pow(hp->kappa,2) + pow(hp->omega_c,2) + 2*hp->kappa*hp->omega_c;
+    hp->a[0] = pow(hp->kappa,2)/hp->delta;
+    hp->a[1] = (-2*pow(hp->kappa,2))/hp->delta; 
+    hp->a[2] = pow(hp->kappa,2)/hp->delta;
+    hp->b[0] = (-2*pow(hp->kappa,2)+2*pow(hp->omega_c,2))/hp->delta;
+    hp->b[1] = (-2*hp->kappa*hp->omega_c + pow(hp->kappa,2) + pow(hp->omega_c,2))/hp->delta;
+
+}
+
+void hp_process(float* inSample, float* outSample, Highpass hp, float* w){
+    w[0] = inSample + (hp->b[0] * w[1]) + (hp->b[1] * w[2]);
+    outSample = (hp->a[0] * w[0]) + (hp->a[1] * w[1]) + (hp->a[2] * w[2]);
+    w[2] = w[1]; 
+    w[1] = w[0];
+}
+
+int main (){
+    
+    float signal[6] = {1.2, 2.2 ,3.2 ,6.4 ,2.4 ,7.5};
+    float delayline[3] = {0,0,0};
+    float output[6] = {0,0,0,0,0,0};
+
+    Lowpass lp;
+    Highpass hp;
+     
+    printf("fc für lowpass eingeben: \n");
+    scanf("%f", &lp.f_c);
+    calc_lowpass(&lp);
+    printf("fc für highpass eingeben: \n");
+    scanf("%f", &hp.f_c);
+    calc_highpass(&hp);
+    
+    printf("%f\n", lp.a[0]);
+    printf("%f\n", lp.a[1]);
+    printf("%f\n", lp.a[2]);
+    printf("%f\n", lp.b[0]);
+    printf("%f\n", lp.b[1]);
+
+    printf("a[0]: %f\n", hp.a[0]);
+    printf("a[1]: %f\n", hp.a[1]);
+    printf("a[2]: %f\n", hp.a[2]);
+    printf("b[0]: %f\n", hp.b[0]);
+    printf("b[1]: %f\n", hp.b[1]);
+
+    for (int i = 0; i<6; i++) hp_process(&signal[i], &output[i], hp, delayline);
+    
+    printf("signal: ");
+    for (int i = 0; i<6; i++)printf("%f", signal[i]);
+    printf("processed signal: ");
+    for (int i = 0; i<6; i++) printf("%f", output[i]);
+
+
+    
+    
+}
